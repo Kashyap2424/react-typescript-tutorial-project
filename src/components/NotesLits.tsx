c9a00903a9fcdd45bbf1280706ac5ec31264aa9b
Kashyap Kapdi <kapdikashyap2003@gmail.com>
2022-03-23 18:37:51 +0530

    Add Delete functionality in notes for deleting a note.

--- src/components/NotesLits.tsx
+++ src/components/NotesLits.tsx
@@ -5,12 +5,22 @@ import { Notes } from "../model/note.model";
 
 interface INotesListProps {
   Notes: Notes[];
+  setNotes: React.Dispatch<React.SetStateAction<Notes[]>>;
 }
 
-const NotesList: React.FunctionComponent<INotesListProps> = ({ Notes }) => {
+const NotesList: React.FunctionComponent<INotesListProps> = ({
+  Notes,
+  setNotes,
+}) => {
+  const handleDelete = (id: String) => {
+    setNotes(Notes.filter((note) => note.id !== id));
+  };
+
   const renderNotes = (): JSX.Element[] => {
     return Notes.map((note) => {
-      return <Note note={note} />;
+      return (
+        <Note key={`${note.id}`} note={note} handleDelete={handleDelete} />
+      );
     });
   };
 
